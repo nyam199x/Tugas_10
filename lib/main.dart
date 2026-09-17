4327d7a116a4e91c4fe479b4caa0c25f827b7627
nyamnyam042 <201212019ilhamramadhan@gmail.com>
2026-09-17 12:56:14 +0700

    selesai

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 // import 'package:tugas_3/Tugas_5/Tugas_3.dart';
-// import 'package:tugas_3/Tugas_5/Tugas_4.dart';
+import 'package:tugas_3/Tugas_5/Tugas_4.dart';
 
 // import 'package:tugas_3/Layout/Tugas_2.dart';
 // import 'package:tugas_3/Loginscreen/Login.dart';
@@ -9,12 +9,12 @@ import 'package:flutter/material.dart';
 // import 'package:tugas_3/Tugas_9/listview.dart';
 // import 'package:tugas_3/tugas_8/bottomnavigationbar.dart';
 // import 'package:tugas_3/Tugas_10/Pendaftaran.dart';
-import 'package:tugas_3/tugas_11/preference_handle.dart';
-import 'package:tugas_3/tugas_11/SplashScreen.dart';
+// import 'package:tugas_3/tugas_11/preference_handle.dart';
+// import 'package:tugas_3/tugas_11/SplashScreen.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await PreferenceHandler.init();
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await PreferenceHandler.init();
   runApp(const MyApp());
 }
 
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const SplashScreenDay15(),
+      home: const listview(),
       debugShowCheckedModeBanner: false,
     );
   }
